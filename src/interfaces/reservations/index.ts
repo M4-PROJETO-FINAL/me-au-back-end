037a31a29bf48c9d34b7706a8d077da4a83d3302
Nicholas Engelbert <nicholas.ge1301@gmail.com>
2022-11-01 10:14:24 -0300

    fix: GET /rooms returns room_type_id

--- src/interfaces/reservations/index.ts
+++ src/interfaces/reservations/index.ts
@@ -1,15 +1,15 @@
 import { IService } from "./../services/index";
-import { IRoom } from "./../rooms/index";
+import { IPetRoom } from "./../rooms/index";
 
 export interface IReservationRequest {
-	checkin: string;
-	checkout: string;
-	services: IService[];
-	pet_rooms: IRoom[];
+  checkin: string;
+  checkout: string;
+  services: IService[];
+  pet_rooms: IPetRoom[];
 }
 
 export interface IReservation extends IReservationRequest {
-	id: string;
-	created_at: string;
-	updated_at: string;
+  id: string;
+  created_at: string;
+  updated_at: string;
 }
