b09160b2bcdecde9c48d6f55565a4756cee17a03
Guilherme Wustro <gui.wustro@gmail.com>
2022-11-03 12:27:11 -0300

    feat: add deploy api

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,25 +1,41 @@
-import { DataSource } from 'typeorm';
-import 'dotenv/config';
+import { DataSource } from "typeorm";
+import "dotenv/config";
 
 const AppDataSource = new DataSource(
-	process.env.NODE_ENV === 'test'
+	process.env.NODE_ENV === "test"
 		? {
-				type: 'sqlite',
-				database: ':memory:',
-				entities: ['src/entities/*.ts'],
+				type: "sqlite",
+				database: ":memory:",
+				entities: ["src/entities/*.ts"],
 				synchronize: true,
 		  }
 		: {
-				type: 'postgres',
-				host: process.env.DB_HOST,
-				port: 5432,
-				username: process.env.DB_USER,
-				password: process.env.DB_PASSWORD,
-				database: process.env.DB,
+				type: "postgres",
+				url: process.env.DATABASE_URL,
+				ssl:
+					process.env.NODE_ENV === "production"
+						? { rejectUnauthorized: false }
+						: false,
 				logging: true,
 				synchronize: false,
-				entities: ['src/entities/*.ts'],
-				migrations: ['src/migrations/*.ts'],
+				entities:
+					process.env.NODE_ENV === "production"
+						? ["dist/src/entities/*.js"]
+						: ["src/entities/*.ts"],
+				migrations:
+					process.env.NODE_ENV === "production"
+						? ["dist/src/migrations/*.js"]
+						: ["src/migrations/*.ts"],
+				// type: 'postgres',
+				// host: process.env.DB_HOST,
+				// port: 5432,
+				// username: process.env.DB_USER,
+				// password: process.env.DB_PASSWORD,
+				// database: process.env.DB,
+				// logging: true,
+				// synchronize: false,
+				// entities: ['src/entities/*.ts'],
+				// migrations: ['src/migrations/*.ts'],
 		  }
 );
 
