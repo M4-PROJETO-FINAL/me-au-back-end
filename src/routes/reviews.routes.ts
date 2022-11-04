d1ce93efbd2ad7d17b2c04a2c6a994b539647292
HigorSkw <higor.skowronski@gmail.com>
2022-11-04 13:40:02 -0300

    fix: update tests

--- src/routes/reviews.routes.ts
+++ src/routes/reviews.routes.ts
@@ -1,18 +1,19 @@
-import { authUser } from '../middlewares/authUser.middleware';
-import { validateReviewCreate } from '../middlewares/validateReviewCreate.middleware';
-import { Router } from 'express';
-import reviewCreateController from '../controllers/reviews/reviewCreate.controller';
-import reviewGetController from '../controllers/reviews/reviewGet.controller';
-import reviewEditController from '../controllers/reviews/reviewUpdate.controller';
-import reviewDeleteController from '../controllers/reviews/reviewDelete.controller';
+import { authUser } from "../middlewares/authUser.middleware";
+import { validateReviewCreate } from "../middlewares/validateReviewCreate.middleware";
+import { validateReviewEdit } from "../middlewares/validateReviewEdit.middleware";
+import { Router } from "express";
+import reviewCreateController from "../controllers/reviews/reviewCreate.controller";
+import reviewGetController from "../controllers/reviews/reviewGet.controller";
+import reviewEditController from "../controllers/reviews/reviewUpdate.controller";
+import reviewDeleteController from "../controllers/reviews/reviewDelete.controller";
 
 const routes = Router();
 
 export const reviewRoutes = () => {
-	routes.post('', authUser, validateReviewCreate, reviewCreateController);
-	routes.get('', reviewGetController);
-	routes.delete('/:id', authUser, reviewDeleteController);
-	routes.patch('/:id', authUser, reviewEditController);
+  routes.post("", authUser, validateReviewCreate, reviewCreateController);
+  routes.get("", reviewGetController);
+  routes.delete("/:id", authUser, reviewDeleteController);
+  routes.patch("/:id", authUser, validateReviewEdit, reviewEditController);
 
-	return routes;
+  return routes;
 };
